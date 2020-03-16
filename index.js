0c5506e0e5958098aaeb37d10d5def3a2b97d06f
Bryant Sell <bryantsell@Bryants-iMac.local>
2020-03-16 08:31:44 -0400

    Add priettierrc. Add more charts to show temp, percip, and wind

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const { menubar } = require("menubar");
+const { menubar } = require('menubar');
 
 const mb = menubar({
-  browserWindow: { height: 675, width: 800 },
-  preloadWindow: true
+    browserWindow: { height: 1550, width: 800 },
+    preloadWindow: true,
 });
 
 const OneMinuteInMilliSeconds = 1000 * 60;
-mb.on("ready", () => {
-  console.log("window is shown");
-  setInterval(() => {
-    console.log("window is reloaded");
-    mb.window.loadURL(`file://${__dirname}/index.html`);
-  }, OneMinuteInMilliSeconds * 5);
+mb.on('ready', () => {
+    console.log('window is shown');
+    setInterval(() => {
+        console.log('window is reloaded');
+        mb.window.loadURL(`file://${__dirname}/index.html`);
+    }, OneMinuteInMilliSeconds * 5);
 });
